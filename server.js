74c07f0a401a7b3b9f2284e6454537058b56770c
Thanh Tung Nguyen <nttung@DESKTOP-ODDIPRD.localdomain>
2020-11-09 07:55:00 +0700

    #added add docker-compose file and connect to database mongodb

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const { log } = require('console');
 const logger = require('./utils/winston')
 const dotenv = require('dotenv')
+const mongoose = require('mongoose')
 
 dotenv.config();
+
+const db = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+mongoose
+	.connect(db, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+	})
+	.then((con) => {
+		logger.info('Connect to database on port=27017')
+	});
 
 const app = require('./app');
 
 const port = process.env.PORT || 5000;
 
 const server = app.listen(port, () => {
-    logger.info(`App running on ${port}...`)
+    logger.info(`App running on port=${port} ...`)
 })
