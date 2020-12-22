a5f7c67c59566832cf9a39186acb44a7d78f38ee
Thanh Tung Nguyen <nttung99qn@gmail.com>
2020-12-22 10:31:46 +0700

    #added post user profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,6 +7,17 @@ module.exports.getAccount = (req, res) => {
     })
 }
 
-module.exports.postAccount = (req, res) => {
+module.exports.postAccount = (req, res, next) => {
+    const updateInfo = req.body;
+    const user = User.findByIdAndUpdate(req.user.id, updateInfo);
     
+    if(!user){
+        next(new AppError('Update user not successful', 404));
+    }
+    
+    res.status(200).render('user-profile', {
+        banner: `User profile`,
+        user: req.user,
+        type: true
+    })
 }
